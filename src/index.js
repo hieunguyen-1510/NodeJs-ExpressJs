6d10fb707617df5240d911bf6c07744a19b50fea
Your Name <you@example.com>
2024-05-26 23:08:04 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ const path = require("path");
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const { log } = require("console");
 const app = express();
 
 const port = 3000;
-
-// HTTP logger
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(morgan('combined'));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+//XMLHttpRequest , fetch, axios,
+// HTTP logger
+//app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars.engine({
@@ -21,7 +26,15 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 app.get('/news', (req, res) => {
+  console.log(req.query.q);
   res.render('news');
+});
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('');
 });
 
 app.listen(port, () => {
