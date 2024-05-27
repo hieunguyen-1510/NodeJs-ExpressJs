0beeee8f1872673e25d73e0dbca1012634c00cd5
Your Name <you@example.com>
2024-05-27 23:42:29 +0700

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ const { log } = require("console");
 const app = express();
 
 const port = 3000;
+
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({
   extended: true
 }));
@@ -22,20 +26,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  console.log(req.query.q);
-  res.render('news');
-});
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send('');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
