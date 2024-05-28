6218445bc7d84fe5a70c173b5e9b85500b327215
Your Name <you@example.com>
2024-05-28 22:13:56 +0700

    Install prettier, lint-staged, husky

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,11 @@
-
 class SiteController {
     // [GET] /home
-    index(req, res){
+    index(req, res) {
         res.render('home');
-
     }
     //[GET] / search
-    search(req, res){
+    search(req, res) {
         res.render('search');
-
     }
 }
-module.exports = new SiteController;
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const path = require("path");
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
-const { log } = require("console");
+const { log } = require('console');
 const app = express();
 
 const port = 3000;
@@ -11,24 +11,29 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 //XMLHttpRequest , fetch, axios,
 // HTTP logger
 //app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+       app.set('view engine', 'hbs');
+        app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
-route(app);
+     route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+     app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
