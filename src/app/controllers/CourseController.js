cf939fd548b87479f46330903330cf6a3d8fb181
Your Name <you@example.com>
2024-07-25 01:45:22 +0700

    update

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -13,6 +13,24 @@ class CourseController {
         .catch(next);
        
     }
+    // [GET] /courses/create
+    create(req, res,next) {
+       res.render('courses/create');
+       
+    }
+    // [POST] /courses/store
+    store(req, res,next) {
+        // res.json(req.body);
+        const formData = req.body;
+        formData.image = `https://files.fullstack.edu.vn/f8-prod/courses/${req.body.videoId}/6.png`;
+        const course = new Course(formData);
+        course.save()
+        .then(course => res.redirect('/'))
+        .catch(error =>{
+
+        });
+     }
+    
 }
 
 module.exports = new CourseController();
